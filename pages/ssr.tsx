1c864a9b05e3eb428f7735137f6654020438aff4
Alexandre Pernin <alexandrep@theodo.fr>
2022-11-18 16:15:41 +0100

    feat: add system info log

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -7,7 +7,26 @@ const Home = ({ toto }: Props) => {
 };
 
 export const getServerSideProps = () => {
+  const os = require("os");
+
   console.log({ test: "log ssr" });
+  const info = {
+    arch: os.arch(),
+    cpus: os.cpus(),
+    endianness: os.endianness(),
+    freemem: os.freemem(),
+    homedir: os.homedir(),
+    hostname: os.hostname(),
+    loadavg: os.loadavg(),
+    networkInterfaces: os.networkInterfaces(),
+    platform: os.platform(),
+    release: os.release(),
+    type: os.type(),
+    uptime: os.uptime(),
+    userInfo: os.userInfo(),
+    version: os.version(),
+  };
+  console.log("system info", info);
   return { props: { toto: "toto" } };
 };
 
